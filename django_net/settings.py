2f0c46b8d6c83c422ff524c9be8eba420b4d8286
Evan Kim <ekim67@alumni.stanford.edu>
2024-01-25 14:11:27 -0800

    railway update

--- django_net/settings.py
+++ django_net/settings.py
@@ -78,10 +78,22 @@ WSGI_APPLICATION = 'django_net.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}'''
+
+DATABASES = {
+    'default': {
+        #'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'BDcgbCcf1gBFg-fgAc2BFAAff6g32B*2',
+        'HOST': 'viaduct.proxy.rlwy.net',
+        'PORT': '18704',
     }
 }
 
@@ -124,8 +136,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
