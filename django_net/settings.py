30a83bdd4a156e903316652d8ba5533d3d7c401a
Evan Kim <ekim67@alumni.stanford.edu>
2024-01-25 14:39:59 -0800

    debug

--- django_net/settings.py
+++ django_net/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qm=2t0+oa)!a(3+q2ukx&w+6*+drmuym^gc+!m-8l1bqx51*7m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
