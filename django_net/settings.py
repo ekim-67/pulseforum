b94cb97fcd187a7f766ab9da0f9daa6e744d6239
Evan Kim <ekim67@alumni.stanford.edu>
2024-01-26 12:30:32 -0800

    debug

--- django_net/settings.py
+++ django_net/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = 'django-insecure-qm=2t0+oa)!a(3+q2ukx&w+6*+drmuym^gc+!m-8l1bqx51*7m'
 
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-qm=2t0+oa)!a(3+q2ukx&w+6*+drmuym^gc+!m-8l1bqx51*7m')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
