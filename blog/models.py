49aeffa162b116941691e6e327f697de4d43b839
Evan Kim <evankim@evans-air.lan>
2024-12-24 15:34:35 -0800

    typo

--- blog/models.py
+++ blog/models.py
@@ -7,7 +7,7 @@ class Club(models.Model):
     name = models.CharField(max_length=100)
     current_book = models.CharField(max_length=100, null=True)
     description = models.CharField(max_length=500, null=True)
-    banner = models.ImageField(default='default_book.jpg', upload_to='profile_pics')
+    banner = models.ImageField(default='default-icon.jpg', upload_to='profile_pics')
     owner = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
