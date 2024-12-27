beaa61bb51b8e9d9d92cb8ce7900ab6633832895
Evan Kim <evankim@evans-air.lan>
2024-12-27 15:08:37 -0800

    asdf

--- blog/views.py
+++ blog/views.py
@@ -83,7 +83,7 @@ def update_club(request):
 class ClubUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Club
     fields = ['name', 'description', 'banner']
-    template_name_suffix = "_update_form.html"
+    template_name_suffix = "_update_form"
 
     def form_valid(self, form):
         form.instance.author = self.request.user
