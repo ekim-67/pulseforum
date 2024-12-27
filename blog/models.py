b7d47a0bfbdb90fff832cf7220c56b8d5b7179b1
Evan Kim <evankim@evans-air.lan>
2024-12-26 16:54:09 -0800

    fix create clubs bug

--- blog/models.py
+++ blog/models.py
@@ -8,7 +8,7 @@ class Club(models.Model):
     current_book = models.CharField(max_length=100, null=True)
     description = models.CharField(max_length=500, null=True)
     banner = models.ImageField(default='default-icon.jpg', upload_to='profile_pics')
-    owner = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.name
