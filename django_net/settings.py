deb8e400698db941f1a5ab679ca1e887729854b1
Evan Kim <evankim@evans-air.lan>
2024-12-27 19:23:23 -0800

    bug

--- django_net/settings.py
+++ django_net/settings.py
@@ -130,7 +130,6 @@ USE_TZ = True
 MEDIA_URL = "media/"
 MEDIA_ROOT = os.environ["RAILWAY_VOLUME_MOUNT_PATH"]
 
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
