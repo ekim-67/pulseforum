925f7cce0c6fdd916aa19bb94c722b946f8130af
Evan Kim <evankim@evans-air.lan>
2024-12-27 20:49:44 -0800

    club banner image

--- blog/views.py
+++ blog/views.py
@@ -13,7 +13,6 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required 
 from django.shortcuts import get_object_or_404
 
-
 def home(request):
     club = get_object_or_404(Club, pk=8)
 
