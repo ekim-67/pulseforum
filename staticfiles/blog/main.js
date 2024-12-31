409e6b4e00af2aea41ae3eca439bc5058a65d73b
Evan Kim <evankim@evans-air.lan>
2024-12-30 16:34:51 -0800

    debug css

--- staticfiles/blog/main.js
+++ staticfiles/blog/main.js
@@ -1,16 +1,3 @@
-/*document.addEventListener('DOMContentLoaded', () => {
-    const clickableDivs = document.querySelectorAll('.media-body[data-url]');
-
-    clickableDivs.forEach(div => {
-        div.addEventListener('click', () => {
-            const url = div.getAttribute('data-url');
-            if (url) {
-                window.location.href = url;
-            }
-        });
-    });
-});*/
-
 document.addEventListener('DOMContentLoaded', () => {
     const container = document.body; // Or use a specific parent container if needed
 
