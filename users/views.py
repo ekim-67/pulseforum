2a137014c259f81aebc33626b0f7f210caefd7bb
Evan Kim <evankim@evans-air.lan>
2025-01-05 12:34:28 -0800

    join navbar

--- users/views.py
+++ users/views.py
@@ -17,7 +17,7 @@ def register(request):
             return redirect('login')
     else:   
         form = UserRegisterForm()
-    return render(request, 'users/register.html', {'form':form})
+    return render(request, 'users/register.html', {'form':form, 'active_page': 'join'})
 
 
 @login_required
